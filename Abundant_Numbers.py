206b0311a8eebc383f572fb6da0ff128a4d01cf9
sai veeranna manikanta <84730664+sai123-tech@users.noreply.github.com>
2022-10-31 20:35:20 -0700

    n=int(input()) x=[] sum1=0 for i in range(1,n):     if n%i==0:         x.append(i) for i in x:     sum1=sum1+i if sum1>n:     print(True) else:     print(False)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,9 +1,12 @@
 n=int(input())
-s=0
+x=[]
+sum1=0
 for i in range(1,n):
     if n%i==0:
-         s+=i
-if s>i:
-    print('True')
+        x.append(i)
+for i in x:
+    sum1=sum1+i
+if sum1>n:
+    print(True)
 else:
-    print('False')
+    print(False)
